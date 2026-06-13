01aeeb03a8468353a413a57e3b67bbd77ab02165
Md Javed <mdjaved17@users.noreply.github.com>
2026-06-13 11:45:57 +0530

    Implemented authentication module with register and login APIs

---
+++ backend/models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    role: {
+      type: String,
+      enum: [
+        "ADMIN",
+        "SALES_USER",
+        "PURCHASE_USER",
+        "MANUFACTURING_USER",
+        "INVENTORY_MANAGER",
+        "BUSINESS_OWNER",
+      ],
+      default: "SALES_USER",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
